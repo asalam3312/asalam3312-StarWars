b4499570961e50e29fa79998d3f40bf837dcecc4
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-04-10 20:46:30 +0000

    characters mapped

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -11,18 +11,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 					title: "SECOND",
 					background: "white",
 					initial: "white"
-				}
-			]
+				},
+			],
+			character: [],
+			planets: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			
+					fetchData: async()=>{
+					let store = getStore()
+					try{
+						const response = await fetch("https://www.swapi.tech/api/people");
+						if(!response.ok){
+							throw new Error(response.statusText)
+						}
+						const data = await response.json()
+						const {resoult} = data;
+						if(resoult && resoult.lenght > 0){
+							const {name, gender, hair_color, eye_color}= resoult[0]
+							setCharactersData({name, gender, hair_color, eye_color})
+						}
+						console.log(data)
+						setStore({character: [...store.character, data]})
+					}catch(e){
+						console.error("An error happended fetching characters data", e)
+					}
 			},
 			changeColor: (index, color) => {
 				//get the store
